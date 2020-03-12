6f3bc64208453f5c318eb459cfef5b71b0030c35
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-03-12 19:24:18 +0100

    add support for decorator

---
+++ src/Decorator/Presenter/HtmlPresenter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Yoeunes\Notify\Decorator\Presenter;
+
+use Yoeunes\Notify\Envelope\Envelope;
+
+final class HtmlPresenter implements PresenterInterface
+{
+    public function render(Envelope $envelope)
+    {
+        // TODO: Implement render() method.
+    }
+
+    public function support(Envelope $envelope, array $context = array())
+    {
+        // TODO: Implement support() method.
+    }
+}
---
+++ src/Decorator/Presenter/PresenterInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Yoeunes\Notify\Decorator\Presenter;
+
+use Yoeunes\Notify\Envelope\Envelope;
+
+interface PresenterInterface
+{
+    /**
+     * @param \Yoeunes\Notify\Envelope\Envelope $envelope
+     *
+     * @return mixed
+     */
+    public function render(Envelope $envelope);
+
+    /**
+     * @param \Yoeunes\Notify\Envelope\Envelope $envelope
+     * @param array                             $context
+     *
+     * @return bool
+     */
+    public function support(Envelope $envelope, array $context = array());
+}
